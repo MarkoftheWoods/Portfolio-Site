51579afa894de0126ab0208294c8e6f9b0a5ad24
Mark <markbwoods@gmail.com>
2020-07-22 21:30:20 -0500

    Create index.php
    Created barebones template for index.php. Contains a basic navbar(with non-functional links) for a header, and placeholder text for main body and footer.

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+  /*******************
+  * Personal Portfolio Site
+  * Name:     Mark Woods
+  * Date:     July 22, 2020
+  ********************/ 
+
+  $pageTitle = "Mark Woods, Developer in training";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title><?= $pageTitle ?></title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <div id="navbar">
+            <ul>
+                <li><a href='aboutme.php'>About Me</a></li>   
+                <li><a href='projects.php'>Projects</a></li>
+                <li><a href='education.php'>Education</a></li>        
+            </ul>
+        </div>
+    </header>
+
+    <section id="main">
+        <div>
+            <p>This is placeholder text.</p>
+        </div>
+    </section>
+
+    <footer>
+        <div>
+            <p>This is the bottom of the page</p>
+        </div>
+    </footer>
+</body>
+</html>
